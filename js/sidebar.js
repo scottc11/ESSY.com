f02592451da7d04b2813cd3d094b60e3723e3d1c
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-05-29 12:27:37 -0400

    centered and sized the project display window proportionate to browser and sidebar

--- js/sidebar.js
+++ js/sidebar.js
@@ -9,6 +9,7 @@ function collapseSidebar() {
   $('.projects-sidebar').removeClass('expand');
   $('.modalCover').removeClass('modalTransition');
   $('.sidebar-open-button').removeClass('openButtonTransition');
+  $('.projectWindow').removeClass('projectWindowTransition');
 }
 
 
