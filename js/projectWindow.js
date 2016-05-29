5644236f5bd157bbc46ecd87b168cedb03416887
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-05-28 23:58:45 -0400

    created a button of vertical text which opens the projects sidebar

--- js/projectWindow.js
+++ js/projectWindow.js
@@ -2,13 +2,15 @@
 function expandProjectsContainer() {
   $('.projects-sidebar').addClass('expand');
   $('.modalCover').addClass('modalTransition');
+  $('.projects-button').addClass('projectsButtonTransition');
 }
 
 function collapseProjectsContainer() {
   $('.projects-sidebar').removeClass('expand');
   $('.modalCover').removeClass('modalTransition');
+  $('.projects-button').removeClass('projectsButtonTransition');
 }
 
 
-$('.button1').on('click', expandProjectsContainer);
+$('.projects-button').on('click', expandProjectsContainer);
 $('.button2').on('click', collapseProjectsContainer);
