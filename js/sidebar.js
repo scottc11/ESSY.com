bd0d740bff71dabde42e63e4710ea591f8846cab
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-05-29 11:22:37 -0400

    changed some file names for organization.

---
+++ js/sidebar.js
@@ -0,0 +1,16 @@
+
+function expandSidebar() {
+  $('.projects-sidebar').addClass('expand');
+  $('.modalCover').addClass('modalTransition');
+  $('.sidebar-open-button').addClass('openButtonTransition');
+}
+
+function collapseSidebar() {
+  $('.projects-sidebar').removeClass('expand');
+  $('.modalCover').removeClass('modalTransition');
+  $('.sidebar-open-button').removeClass('openButtonTransition');
+}
+
+
+$('.sidebar-open-button').on('click', expandSidebar);
+$('.sidebar-close-button').on('click', collapseSidebar);
