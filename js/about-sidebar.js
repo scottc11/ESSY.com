f1b27b896e47a70b9cd325a8058ea2aeffd99668
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-05-30 02:27:07 -0400

    changed the about window transition and its location. It now expands from the middle outward.

--- js/about-sidebar.js
+++ js/about-sidebar.js
@@ -1,10 +1,17 @@
 function expandAbout() {
-  $('.about-sidebar').addClass('about-window-transition')
+  $('.about-sidebar').addClass('about-window-transition');
+  $('.modalCover').addClass('modalTransition');
 }
 
 function collapseAbout() {
-  $('.about-sidebar').removeClass('about-window-transition')
+  $('.about-sidebar').removeClass('about-window-transition');
+  $('.modalCover').removeClass('modalTransition');
 }
 
 $('.about-open-button').on('click', expandAbout);
 $('.about-close-button').on('click', collapseAbout);
+
+// TRANSITION END BUTTON DISSAPEAR
+// $('about-sidebar').on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
+//
+// });
