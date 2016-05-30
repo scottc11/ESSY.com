5781875e86d36ca70b9ad9cd93e970c302c320a9
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-05-30 12:18:27 -0400

    added some styling to the page h1 and created some media queries to change font size

--- js/about-sidebar.js
+++ js/about-sidebar.js
@@ -13,15 +13,20 @@ function collapseAbout() {
 $('.about-open-button').on('click', expandAbout);
 $('.about-close-button').on('click', collapseAbout);
 
-// On about windows open transition, slide the side buttons out of view
+// When the 'about' windows open transition ends, slide the side buttons out of view
 $('.about-sidebar').on("transitionend webkitTransitionEnd oTransitionEnd MSTransitionEnd", function() {
   var $about = $('.about-open-button');
   var $sidebar = $('.sidebar-open-button');
+  var $logo = $('.logo');
+  var $text = $('.pageHeading > h1');
   if ($about.hasClass('aboutButtonTransition')) {
     $about.removeClass('aboutButtonTransition');
-    $sidebar.removeClass('openButtonTransition');
+    $sidebar.removeClass('openButtonTransition'); //button transition
+    $logo.removeClass('blur-me');  //un-blur the logo
   } else {
     $about.addClass('aboutButtonTransition');
     $sidebar.addClass('openButtonTransition');
+    $logo.addClass('blur-me'); // blur logo
+    $text.addClass('blurry-text'); // blur heading text
   }
 });
