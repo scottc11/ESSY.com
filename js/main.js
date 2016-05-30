3cc7252cbb51aab15d1b9251dbfaa856faa1daed
Scott Campbell <scottcampbell@Scotts-MacBook-Pro.local>
2016-05-30 19:41:43 -0400

    created a loading animation - which does half of what it is suppose to.

--- js/main.js
+++ js/main.js
@@ -1,17 +1,7 @@
 
-$(window).on('scroll', function() {
-  var $window = $(this);
-  // returns number of pixels scrolled
-  var pxScrolled = $window.scrollTop();
-  // console.log(pxScrolled);
-
-  var sketchHeight = $('#canvas-container').height();
-
-})
-
 // Preloader for main page
 // Wait for window load
 $(window).load(function() {
 	// Animate loader off screen
-	$(".preloader").fadeOut("slow");;
+	$(".preloaderContainer").fadeOut("slow");;
 });
